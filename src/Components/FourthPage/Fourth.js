b8bd9fdea4f1b324bdc1f1e0e662a7cd670efb98
Shota <shotikoavaliani97@gmail.com>
2022-07-08 18:48:05 +0400

    fourth page css part is done, but i have left something javascript logic on second and third component

--- src/Components/FourthPage/Fourth.js
+++ src/Components/FourthPage/Fourth.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Header from '../FirstPage/Header'
+import "../FourthPage/Fourth.css"
+import Fourthimg from "../FourthPage/FourthAssets/fourth.png"
 
 function Fourth() {
   return (
-    <div>Fourth</div>
+    <div className='fourth'>
+        <div className='fourth1'>
+            <Header />
+            <img src={Fourthimg} alt="fourth" />
+        </div>
+        <div className='fourth2'>
+            <h2> Onboarding completed! </h2>
+        </div>
+    </div>
   )
 }
 
